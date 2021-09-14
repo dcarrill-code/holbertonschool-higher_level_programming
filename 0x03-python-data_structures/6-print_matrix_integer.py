71dc2ff7b39c59f07c95cf682c22781e373ff3ab
dcarrill-code <3346@holbertonschool.com>
2021-09-13 22:04:13 -0500

    Debugging

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
             if i is not row[len(row) - 1]:
                 print("{:d}".format(i), end=" ")
             else:
-                print("{:d:".format(i), end=" ")
+                print("{:d:".format(i), end="")
         print()
