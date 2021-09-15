a2bb0b6981ba0323eed2e8f509226fd36002332b
dcarrill-code <3346@holbertonschool.com>
2021-09-14 22:45:41 -0500

    Catching up

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is not None:
+        new = []
+        for row in matrix:
+            new.append(list(map(lambda x: x**2, rows)))
+            return (new)
+        return None
